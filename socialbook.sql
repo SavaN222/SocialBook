cdfef204e0f975724c5a20fee395f310cfffb2a1
SavaN222 <nakaradasava@gmail.com>
2020-01-29 14:49:18 +0100

    Database updated

--- socialbook.sql
+++ socialbook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 26, 2020 at 06:47 PM
+-- Generation Time: Jan 29, 2020 at 01:48 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -33,30 +33,22 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
+  `post_user` int(11) NOT NULL,
   `description` varchar(255) NOT NULL,
   `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('0','1') NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=70 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=82 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`id`, `user_id`, `post_id`, `description`, `date_added`) VALUES
-(55, 18, 23, 'Great feature', '2020-01-18 13:04:16'),
-(54, 18, 22, 'I agree', '2020-01-17 18:09:12'),
-(53, 19, 22, 'Yes, developer doing amazing job :D', '2020-01-17 18:08:00'),
-(66, 27, 22, 'test', '2020-01-26 01:40:49'),
-(67, 27, 30, 'test\n', '2020-01-26 01:41:05'),
-(61, 19, 25, 'BTW!, gallery is amazing :)', '2020-01-18 14:34:29'),
-(69, 19, 33, 'Very cool post', '2020-01-26 02:34:12'),
-(68, 28, 33, 'Cool guy comment', '2020-01-26 02:33:38'),
-(64, 24, 28, 'wow\n', '2020-01-24 17:08:52'),
-(60, 19, 25, 'Mirko, you have no clue ', '2020-01-18 14:34:03'),
-(59, 18, 25, 'Mr Cat is better than you!!!', '2020-01-18 14:33:29'),
-(58, 20, 23, 'I\'am hungry!', '2020-01-18 13:21:39'),
-(57, 18, 20, 'Okay ', '2020-01-18 13:19:41'),
-(56, 19, 23, 'Ajax is really amazing', '2020-01-18 13:04:55');
+INSERT INTO `comments` (`id`, `user_id`, `post_id`, `post_user`, `description`, `date_added`, `status`) VALUES
+(81, 36, 41, 40, 'Go RANDY!!!', '2020-01-29 14:37:59', '1'),
+(80, 40, 41, 40, '!!!', '2020-01-29 14:37:44', '0'),
+(79, 40, 41, 40, 'Okay', '2020-01-29 14:37:40', '0'),
+(75, 34, 36, 33, 'Sure, Eric!!!', '2020-01-29 14:20:50', '1');
 
 -- --------------------------------------------------------
 
@@ -72,21 +64,16 @@ CREATE TABLE IF NOT EXISTS `friends` (
   `status` enum('0','1','2','') NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`,`friend_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `friends`
 --
 
 INSERT INTO `friends` (`id`, `user_id`, `friend_id`, `status`) VALUES
-(27, 20, 18, '2'),
-(5, 19, 20, '2'),
-(24, 19, 18, '2'),
-(7, 23, 19, '2'),
-(25, 26, 18, '2'),
-(28, 18, 23, '2'),
-(30, 27, 18, '2'),
-(33, 28, 19, '2');
+(44, 40, 36, '2'),
+(45, 40, 33, '2'),
+(41, 33, 34, '2');
 
 -- --------------------------------------------------------
 
@@ -102,17 +89,18 @@ CREATE TABLE IF NOT EXISTS `gallery` (
   `description` varchar(255) NOT NULL,
   `status` enum('0','1') NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `gallery`
 --
 
 INSERT INTO `gallery` (`id`, `user_id`, `photo`, `description`, `status`) VALUES
-(4, 20, 'images/gallery/TalkingDogimg1579353948.jpg', 'I\'am coding dog.\r\nThis is test for upload photos to profile gallery, design is very bad but i don\'t worry to much, because my main focus is Back-End logic with PHP. Some random text, dummy text, dummy,.............. :D eee test for hacking alert(\'TEST\')', '1'),
-(5, 20, 'images/gallery/TalkingDogcatCoding1579354136.jpg', 'This is my coding friend, MR.Cat. He have best routine for coding, secret is this:\r\n1 line of code is equal to 10 hours of sleep', '1'),
-(14, 19, 'images/gallery/NevenaNenicvintage-1950s-887273_6401580002481.jpg', 'Cool Photo', '1'),
-(15, 28, 'images/gallery/CoolGuypaul-murphy-Ut9yic62qLE-unsplash1580038430.jpg', 'Graffiti', '0');
+(22, 33, 'images/gallery/EricCartmancartmanFunny1580305190.jpg', 'Test', '0'),
+(23, 40, 'images/gallery/RandyMarshnewCover1580305362.jpg', '', '1'),
+(20, 35, 'images/gallery/CoderDogcodeMeme11580302650.png', '', '1'),
+(19, 35, 'images/gallery/CoderDogcodingCat1580302637.jpg', 'This is my friend coding cat, he have best programming routine.\r\n1 line of code is equal to 10 hours of sleep', '1'),
+(18, 35, 'images/gallery/CoderDogdogCover1580302595.png', '', '1');
 
 -- --------------------------------------------------------
 
@@ -127,18 +115,15 @@ CREATE TABLE IF NOT EXISTS `likes` (
   `post_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`,`post_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=64 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `likes`
 --
 
 INSERT INTO `likes` (`id`, `user_id`, `post_id`) VALUES
-(59, 19, 33),
-(62, 28, 33),
-(43, 24, 28),
-(34, 19, 23),
-(40, 18, 25);
+(77, 33, 41),
+(76, 36, 41);
 
 -- --------------------------------------------------------
 
@@ -154,20 +139,20 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `text` varchar(255) NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `messages`
 --
 
 INSERT INTO `messages` (`id`, `user_id`, `friend_id`, `text`, `date`) VALUES
-(1, 18, 19, 'Hey', '2020-01-26 18:11:50'),
-(3, 19, 18, 'Hey', '2020-01-26 18:12:22'),
-(5, 18, 19, 'Check out my gallery', '2020-01-26 18:12:50'),
-(7, 19, 18, 'I will', '2020-01-26 18:13:23'),
-(8, 19, 18, 'Gallery is amazing', '2020-01-26 18:38:59'),
-(12, 19, 18, 'Are you see my posts?', '2020-01-26 19:38:33'),
-(11, 18, 19, 'Thank you', '2020-01-26 19:37:20');
+(32, 40, 36, 'Okay', '2020-01-29 14:41:57'),
+(31, 36, 40, 'Please', '2020-01-29 14:41:49'),
+(30, 36, 40, 'Change your profile pic...!!!!!!!!!!!', '2020-01-29 14:41:43'),
+(28, 40, 36, '???', '2020-01-29 14:41:20'),
+(29, 40, 36, '?', '2020-01-29 14:41:24'),
+(27, 40, 36, 'Hi???', '2020-01-29 14:41:07'),
+(26, 36, 40, 'Hi!', '2020-01-29 14:40:58');
 
 -- --------------------------------------------------------
 
@@ -182,22 +167,16 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `description` varchar(255) NOT NULL,
   `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `posts`
 --
 
 INSERT INTO `posts` (`id`, `user_id`, `description`, `date_added`) VALUES
-(32, 27, 'tasdasdas', '2020-01-26 01:45:57'),
-(25, 20, 'Check out my awesome gallery, AW AW!', '2020-01-18 14:30:49'),
-(24, 20, 'AW AW AW!!', '2020-01-18 13:21:20'),
-(23, 19, 'WOW, NOW WE HAVE COMMENTS!\r\nWrite some comment to test this functionality', '2020-01-17 14:56:25'),
-(22, 18, 'New features on this site is too good, #goodJobTeam', '2020-01-17 02:43:44'),
-(20, 19, 'I\'am new on this social media.\r\nSend me a friend request :D ', '2020-01-17 01:15:25'),
-(28, 23, 'I am new on this social network', '2020-01-22 20:46:24'),
-(33, 28, 'Cool Guy Post', '2020-01-26 02:33:28'),
-(31, 27, 'test', '2020-01-26 01:40:20');
+(41, 40, '1 like and I\'ll get drunk by myself.', '2020-01-29 14:37:32'),
+(37, 35, 'Check out my gallery!', '2020-01-29 13:56:14'),
+(36, 33, 'I\'m not fat, I\'m big-boned', '2020-01-29 13:52:28');
 
 -- --------------------------------------------------------
 
@@ -218,20 +197,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `cover_pic` varchar(255) DEFAULT 'images/cover.png',
   `status` enum('0','1') NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `fname`, `lname`, `gender`, `email`, `password`, `birth_date`, `profile_pic`, `cover_pic`, `status`) VALUES
-(28, 'Cool', 'Guy', 'male', 'cguy@gmail.com', '$2y$10$7gvu4R6CKmmyEco0NIHDq.AUoG/6xWhLsA8ltvyCr7Na20t3VmclO', '1996-08-30', 'images/profile/steve-halama-agPWVfDlApM-unsplash1580002395.jpg', 'images/cover/paul-murphy-Ut9yic62qLE-unsplash1580035438.jpg', '0'),
-(27, 'Jana', 'Doe', 'other', 'jana@gmail.com', '$2y$10$J/.K1kjyZ0kdRDxHbKkhy.l2LXl/eBUrZCvzAlwYNohiXnZfHQ8Bq', '1959-02-19', 'images/profile/vintage-1950s-887273_6401579999178.jpg', NULL, '0'),
-(26, 'Milica', 'Milic', 'female', 'milica@gmail.com', '$2y$10$aQxbFM9n/bL.GsQ0TeV4PO3bNj.bU5BA383dsA3NrC9K6Z9QHjEzO', '2002-04-23', 'images/profile/img1579983850.jpg', NULL, '0'),
-(19, 'Nevena', 'Nenic', 'female', 'nena@gmail.com', '$2y$10$uetmutwotXgYnfMJcDRB6ePGDdVEjMPT9mCHOccIoBn0rlTTZr.vK', '1999-12-23', 'images/profile/michael-dam-mEZ3PoFGs_k-unsplash1579108028.jpg', NULL, '0'),
-(20, 'Talking', 'Dog', 'other', 'dog@gmail.com', '$2y$10$AbdcMZ6bYcnjXk72LAoQXeUNC3Tudr3LhcCes5e54HYK8THC2L8h6', '1992-07-17', 'images/profile/img1579350058.jpg', 'images/cover.png', '0'),
-(23, 'John', 'Doe', 'male', 'jdoe@gmail.com', '$2y$10$yqxxZMFR8vqNtYs1Y/HqzOtP7SQf2TC8ha5BlrOzoepgAkOX7LGt2', '2002-05-12', 'images/profile/mali1579722303.png', NULL, '0'),
-(18, 'Mirko', 'Mirkovic', 'male', 'mirko@gmail.com', '$2y$10$lkDsbVkg40g7NebCuiqQuOVG5TYuUKvyem.sBErSBJ9mpqeNEY1b.', '1998-09-18', 'images/profile/rohan-g-hdzBDVVsRT4-unsplash1579107981.jpg', NULL, '1');
+(40, 'Randy', 'Marsh', 'male', 'rmarsh@gmail.com', '$2y$10$ESCO9rPFU.cxU/rW5SDqw.856aTTMwGIs2bTnt7XcdTpuhGfNIUqi', '1982-02-22', 'images/profile/newProfile1580305338.jpg', 'images/cover/coverRendy1580305015.jpg', '1'),
+(36, 'Mr.', 'Towelie', 'other', 'towelie@gmail.com', '$2y$10$4J7LozyWJnVlWU8zbEg80OJL18yx6Zim6df.6rOulmq9JjEmNN3IC', '1995-02-24', 'images/profile/towelProfile1580302754.jpg', 'images/cover/coverTowel1580302754.jpg', '0'),
+(34, 'Butters', 'Stotch', 'male', 'butters@gmail.com', '$2y$10$VWu6VCbW0osV1Sc66y9hqurmmiuvhZBwITn4C08IsNtqZz9.gCzMm', '1997-08-13', 'images/profile/profileButters1580302404.jpg', 'images/cover/coverButters1580302404.jpg', '0'),
+(35, 'Coder', 'Dog', 'other', 'dog@gmail.com', '$2y$10$z0BJB/h9ngooiBhOuymEZuHJysl.kDiHrlwke6M.cnCczgXWz.R5W', '2001-11-11', 'images/profile/img1580302557.jpg', 'images/cover/dogCover1580302557.png', '0'),
+(33, 'Eric', 'Cartman', 'male', 'ecartman@gmail.com', '$2y$10$ZDIKiY0pYaSEKjGuKkRwxeOFGEH80JRLU53MUlsjrrI8h8iTKL8ky', '1997-08-13', 'images/profile/profileCartman1580302210.jpg', 'images/cover/coverCartman1580302210.jpg', '0');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
